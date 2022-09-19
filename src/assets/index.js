bc8e848e28fbd309b9a8d32d7a3ab553716c87b8
Henriquesoto92 <henriquesoto92@gmail.com>
2022-09-18 23:16:57 -0300

    :bug: fix bug image

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import logoNomeRazor from "./images/logoNomeRazor.svg";
+import logoRazor from "./images/logoRazor.svg";
+
+export { logoRazor, logoNomeRazor };
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,5 @@
 import { Flex, Image, Link } from "@chakra-ui/react";
+import { logoNomeRazor, logoRazor } from "../../assets";
 
 export const Navbar = () => {
   return (
@@ -11,16 +12,8 @@ export const Navbar = () => {
       alignItems="center"
     >
       <Flex justify="start" alignItems="center" gap="10px" w="fit-content">
-        <Image
-          h="60px"
-          src="../../../public/images/logo-razor.svg"
-          alt="logo-razor"
-        />
-        <Image
-          h="40px"
-          src="../../../public/images/logo-nome-razor.svg"
-          alt="logo-nome-razor"
-        />
+        <Image h="60px" src={logoRazor} alt="logo-razor" />
+        <Image h="40px" src={logoNomeRazor} alt="logo-nome-razor" />
       </Flex>
       <Flex gap={["20px", "100px"]}>
         <Link as="a" href="#diferencialid">
