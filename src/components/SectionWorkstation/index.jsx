0a5331c99e0ea44edc295320a01e563d1cdc8021
matheus-vandyke <111263179+matheus-vandyke@users.noreply.github.com>
2022-09-18 22:27:46 -0700

    changed text

--- src/components/SectionWorkstation/index.jsx
+++ src/components/SectionWorkstation/index.jsx
@@ -16,7 +16,7 @@ const workstationArray = [
   {
     image1: "../../../public/images/intel-xeon.png",
     image2: "../../../public/images/epyc.png",
-    text: "Máquinas com processadores de última geração Intel ou AMD e até 64 núcleos. Customize com multiprocessamento de até 2 CPUs por PC e a linha Intel XEON e AMD Threadripper ou AMD Epyc.",
+    text: "Máquinas com processadores de última geração Intel ou AMD e até 64 núcleos. Customize com multiprocessamento de até 2 CPUs por PC e a linha Intel XEON e AMD Epyc.",
     title: "Processador Workaholic",
     highlightTitle: "Processador",
     alt: "processador",
